bd8a0b2491ddbe7710f63988da7d4a85849257ed
kgonzalez1219 <kat@MSI.localdomain>
2020-08-06 22:18:13 -0700

    try new number function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 'use strict'
 
+// random interger from mdn/w3schools
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 // object literals/store locations
 
 var seattle = {
   minCust: 23, 
   maxCust: 65, 
   avgCookiePurch: 6.3,
-  Math.floor(Math.random());
+  
 
 };
 
@@ -14,26 +21,26 @@ var tokyo = {
   minCust: 3,
   maxCust: 24,
   avgCookiePurch: 1.2,
-  Math.floor(Math.random());
+  
 };
 
 var dubai = {
   minCust: 11,
   maxCust: 38,
   avgCookiePurch:3.7,
-  Math.floor(Math.random());
+  
 };
 
 var paris = {
   minCust: 20,
   maxCust: 38,
   avgCookiePurch: 2.3,
-  Math.floor(Math.random());
+  
 };
 
 var Lima = {
   minCust: 2,
   maxCust: 16,
   avgCookiePurch: 4.6,
-  Math.floor(Math.random());
+  
 };
