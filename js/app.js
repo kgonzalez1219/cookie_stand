397992324b93280c3ab27077d1c158ed015563dd
kgonzalez1219 <kat@MSI.localdomain>
2020-08-06 21:03:32 -0700

    establish object literals and properties

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
 'use strict'
 
+// object literals
+
+var seattle = {
+  minCust: 23, 
+  maxCust: 65, 
+  avgCookiePurch: 6.3,
+
+};
+
+var tokyo = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookiePurch: 1.2,
+
+};
+
+var dubai = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookiePurch:3.7,
+
+};
+
+var paris = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookiePurch: 2.3,
+
+};
+
+var Lima = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookiePurch: 4.6,
+
+};
