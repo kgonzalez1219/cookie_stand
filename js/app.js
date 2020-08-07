a74e4ac04337cde7f6a0e53bd398f9dde9248697
kgonzalez1219 <kat@MSI.localdomain>
2020-08-06 21:13:29 -0700

    establish methods

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use strict'
 
-// object literals
+// object literals/store locations
 
 var seattle = {
   minCust: 23, 
   maxCust: 65, 
   avgCookiePurch: 6.3,
+  Math.floor(Math.random());
 
 };
 
@@ -13,26 +14,26 @@ var tokyo = {
   minCust: 3,
   maxCust: 24,
   avgCookiePurch: 1.2,
-
+  Math.floor(Math.random());
 };
 
 var dubai = {
   minCust: 11,
   maxCust: 38,
   avgCookiePurch:3.7,
-
+  Math.floor(Math.random());
 };
 
 var paris = {
   minCust: 20,
   maxCust: 38,
   avgCookiePurch: 2.3,
-
+  Math.floor(Math.random());
 };
 
 var Lima = {
   minCust: 2,
   maxCust: 16,
   avgCookiePurch: 4.6,
-
+  Math.floor(Math.random());
 };
