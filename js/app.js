c0827e26bb841310d162507ecaedc8b8db973402
kgonzalez1219 <kat@MSI.localdomain>
2020-08-14 18:08:37 -0700

    add all store locations

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-//constructors
+//constructor
 
 function Store(name, minCust, maxCust, avgCookiePurch) {
   this.name = name;
@@ -20,25 +20,14 @@ function Store(name, minCust, maxCust, avgCookiePurch) {
   }
 }
 
-var seattle = new Store('seattle', 23, 65, 6.3);
+//stores
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store ('Tokyo', 3, 24, 1.2);
+var dubai = new Store ("Dubai", 11, 38, 3.7);
+var paris = new Store ('Paris', 20, 38, 2.3);
+var lima = new Store ('Lima', 2, 26, 4.6);
 
-console.log(seattle);
-
-/*function Store(name, minCust, maxCust, avgCookiePurch) {
-  
-}
-
-function Store(name, minCust, maxCust, avgCookiePurch) {
-  
-}
-
-function Store(name, minCust, maxCust, avgCookiePurch) {
-  
-}
-
-function Store(name, minCust, maxCust, avgCookiePurch) {
-  
-}
+/*
 
 
 
